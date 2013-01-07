13081f2484c570787b6ca722bbf3a27a18a27148
Paul Bonser <pib@paulbonser.com>
2013-01-07 13:54:51 -0600

    Everything is required for the invoice

--- pennyweb/views/__init__.py
+++ pennyweb/views/__init__.py
@@ -1,7 +1,7 @@
 from pennyweb import app
 
 from flask import flash, redirect, render_template, url_for
-from flask.ext.wtf import Email, Form, TextField, Required
+from flask.ext.wtf import Email, Form, Required, SelectField, TextField
 
 
 class InvoiceForm(Form):
@@ -12,11 +12,14 @@ class InvoiceForm(Form):
     mobile = TextField('mobile phone')
 
     # primary address
-    p_street1 = TextField('address 1')
+    p_street1 = TextField('address 1', [Required()])
     p_street2 = TextField('address 2')
-    p_city = TextField('city')
-    p_state = TextField('state')
-    p_code = TextField('zip')
+    p_city = TextField('city', [Required()])
+    p_state = TextField('state', [Required()])
+    p_code = TextField('zip', [Required()])
+
+    payment_type = SelectField('payment type', default='subscription',
+                               choices=[('', ''), ('', '')])
 
 
 @app.route('/', methods=('GET', 'POST'))
