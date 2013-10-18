0f2565c2e3ad1c6b64a15d515d4b497a9480effc
Paul Bonser <pib@paulbonser.com>
2013-10-17 23:34:16 -0500

    wtforms library changes

--- pennyweb/views/__init__.py
+++ pennyweb/views/__init__.py
@@ -3,26 +3,28 @@ from pennyweb.models import create_invoice, ClientAlreadyExists,\
     verify_callback, payment_callback
 
 from flask import flash, redirect, request, render_template, url_for
-from flask.ext.wtf import Email, Form, Required, TextField
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.validators import DataRequired, Email
 
 
 class InvoiceForm(Form):
     # The Basics
-    first_name = TextField('first name *', [Required()])
-    last_name = TextField('last name *', [Required()])
-    email = TextField('email *', [Required(), Email()])
+    first_name = TextField('first name *', [DataRequired()])
+    last_name = TextField('last name *', [DataRequired()])
+    email = TextField('email *', [DataRequired(), Email()])
     home_phone = TextField('home phone')
     mobile = TextField('mobile phone')
 
     # Primary Address
-    p_street1 = TextField('address 1 *', [Required()])
+    p_street1 = TextField('address 1 *', [DataRequired()])
     p_street2 = TextField('address 2')
-    p_city = TextField('city *', [Required()])
-    p_state = TextField('state *', [Required()])
-    p_code = TextField('zip *', [Required()])
+    p_city = TextField('city *', [DataRequired()])
+    p_state = TextField('state *', [DataRequired()])
+    p_code = TextField('zip *', [DataRequired()])
 
 
-@app.route('/', methods=('GET', 'POST')) # Receive form data and validate
+@app.route('/', methods=('GET', 'POST'))  # Receive form data and validate
 def index():
     form = InvoiceForm()
     if form.validate_on_submit():
@@ -38,13 +40,13 @@ def index():
     return render_template('index.html', form=form)
 
 
-@app.route('/success') # Woo, invoice created!
+@app.route('/success')  # Woo, invoice created!
 def success():
     invoice_url = request.args.get('invoice')
     return render_template('success.html', invoice_url=invoice_url)
 
 
-@app.route('/freshbooks_webhook', methods=('POST',)) # Freshbooks callback
+@app.route('/freshbooks_webhook', methods=('POST',))  # Freshbooks callback
 def freshbooks_webhook():
     name = request.form.get('name')
     if name == 'callback.verify':
