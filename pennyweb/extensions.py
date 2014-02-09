6237461e44653b9e5f50e70b122dac10403f8068
Paul Bonser <pib@paulbonser.com>
2014-02-08 23:02:14 -0600

    fully set up flask-security login, register, recover password, etc

--- pennyweb/extensions.py
+++ pennyweb/extensions.py
@@ -1,6 +1,7 @@
 from . import app
 from . import assets
 from flask.ext.debugtoolbar import DebugToolbarExtension
+from flask.ext.mail import Mail
 
-# Debug toolbar
 toolbar = DebugToolbarExtension(app)
+mail = Mail(app)
